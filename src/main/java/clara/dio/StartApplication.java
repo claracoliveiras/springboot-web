f5a6c801bc9457bece434aaf324fcf2d134b0f9d
Clara Campos <claracoliveiras@gmail.com>
2023-10-21 17:24:54 -0300

    Update StartApplication.java

--- src/main/java/clara/dio/StartApplication.java
+++ src/main/java/clara/dio/StartApplication.java
@@ -13,13 +13,8 @@ public class StartApplication implements CommandLineRunner {
     @Autowired
     private UserRepository repository;
 
-    @Value("${spring.config.name}")
-    private String configName;
-
     @Override
     public void run(String... args) throws Exception {
-
-        System.out.println("spring.config.name: " + configName);
         for(User u: repository.findAll()){
             System.out.println(u);
         }
